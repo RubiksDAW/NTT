ce9e4254f4d1cec590b85119b8d0e8e752257c87
alejandro <aledelarosa2@gmail.com>
2022-05-20 10:28:12 +0200

    Codigo formateado

--- nttdatacenters-java-t1-AlejandroRC-2.0/src/main/java/nttdata/javat1/T1MainAlejandroRC.java
+++ nttdatacenters-java-t1-AlejandroRC-2.0/src/main/java/nttdata/javat1/T1MainAlejandroRC.java
@@ -18,27 +18,25 @@ import nttdata.java1.game.Ball;
  *
  */
 public class T1MainAlejandroRC {
-	
+
 	private static final Logger CHIVATO = LoggerFactory.getLogger("padre");
 
-
 	public static void main(String[] args) {
-		
-		/**Variable que se solicita al usuario para seleccionar la opcion del menu */
+
+		/** Variable que se solicita al usuario para seleccionar la opcion del menu */
 		int opcion = 0;
-		
-		
+
 		Scanner sc = new Scanner(System.in);
-		
+
 		/** Coleccion que almacena datos asociando una llave a un valor */
 		Map<String, Integer> tableScore = new HashMap<>();
-		
+
 		System.out.println("Introduce tu NICK");
-		
+
 		Ball p = new Ball(sc.nextLine());
-		
+
 		Game pinball = new Game(p);
-		
+
 		StringBuilder str = new StringBuilder();
 
 		do {
@@ -57,37 +55,36 @@ public class T1MainAlejandroRC {
 
 				System.out.println("¿Cuantas bolas quieres comprar?");
 				validateData(sc, tableScore, p, pinball);
-				
+
 				CHIVATO.debug("Nombre del jugador asociado a la bola {}", p.getNick());
 				CHIVATO.debug("Numero de rebotes obtenidos {}", p.getRandom());
 				CHIVATO.debug("Puntuación obtenida por la el jugador {}", p.getScore());
-				
+
 				str.append(System.getProperty("line.separator"));
 				str.append("Tu puntuación ha sido de: " + p.getScore());
 				str.append(", sigue jugando para batirla");
 				str.append(System.getProperty("line.separator"));
-				
+
 				System.out.println(str);
-				
+
 				break;
 
 			case 2:
-				
+
 				// Utilizamos printf para dar formato a la salida por consola
 				System.out.println("--------------------------------");
 				System.out.printf("%10s %20s", "JUGADOR", "PUNTUACION");
 				System.out.println();
 				System.out.println("--------------------------------");
-				
-				
+
 				for (String key : tableScore.keySet()) {
 
 					System.out.printf("%10s %20s", key, tableScore.get(key));
 					System.out.println();
-					
+
 					CHIVATO.debug("Clave del mapa generada {}", key);
 					CHIVATO.debug("Valor asociado al mapa {}", tableScore.get(key));
-					
+
 				}
 
 				System.out.println("--------------------------------");
@@ -98,10 +95,10 @@ public class T1MainAlejandroRC {
 
 				// Con newGame() reseteamos la instancia p dejando sus valores a nulo
 				pinball.newGame();
-				
+
 				System.out.println("Nick del nuevo jugador:");
 				sc.nextLine();
-				
+
 				p = new Ball(sc.nextLine());
 				System.out.println("¿Cuantas bolas quieres comprar?");
 
@@ -110,11 +107,11 @@ public class T1MainAlejandroRC {
 				break;
 
 			case 4:
-				
+
 				System.out.println("Vuelve pronto!");
 
 				break;
-				
+
 			default:
 				System.out.println("Selecciona una opción válida");
 
@@ -123,15 +120,16 @@ public class T1MainAlejandroRC {
 
 		} while (opcion != 4);
 	}
-	
+
 	/**
 	 * Comprueba que no se introducen valores erroneos por escaner
+	 * 
 	 * @param opcion
 	 * @param sc
 	 * @return
 	 */
 	private static int validateDataMenu(int opcion, Scanner sc) {
-		
+
 		try {
 
 			opcion = sc.nextInt();
@@ -140,12 +138,13 @@ public class T1MainAlejandroRC {
 
 			sc.next();
 		}
-		
+
 		return opcion;
 	}
-	
+
 	/**
-	 * Comprueba que no se añaden valores erroneos 
+	 * Comprueba que no se añaden valores erroneos
+	 * 
 	 * @param sc
 	 * @param tableScore
 	 * @param p
@@ -155,17 +154,17 @@ public class T1MainAlejandroRC {
 	private static void validateData(Scanner sc, Map<String, Integer> tableScore, Ball p, Game pinball) {
 		int bolas;
 		try {
-			
+
 			// Validacion de los datos introducidos
 			bolas = sc.nextInt();
 			pinball.startGame(p, bolas);
 			tableScore.put(p.getNick(), p.getScore());
-			
+
 		} catch (InputMismatchException e) {
-			
+
 			System.out.println("ERROR! Has introducido un dato incorrecto");
 			sc.next();
-			
+
 		}
 	}
 
